b659cafeb8c772630bafbb4d87a484a73a65a539
unknown <s.alireza.m8888@gmail.com>
2020-10-13 13:07:01 +0330

    add style and complete project

--- auto-complete/src/view/Main/Main.jsx
+++ auto-complete/src/view/Main/Main.jsx
@@ -2,18 +2,36 @@ import React, { useState, useEffect } from 'react';
 import useDebounce from '../../myHook/useDebounce'
 import './Main.css'
 
+import { css } from "@emotion/core";
+import { ClipLoader } from "react-spinners";
+import { Container } from 'react-bootstrap';
+
+const override = css`
+  position: absolute;
+  top:8.75rem;
+  right:25.5%;
+  display: block;
+  width:2rem;
+  height:2rem;
+  margin: 0 auto;
+  border: .35rem solid;
+  border-color: #00fff0;
+  border-bottom-color: transparent;
+`;
+
 function Main() {
 
     const [searchTerm, setSearchTerm] = useState('');
     const [results, setResults] = useState([]);
     const [isSearching, setIsSearching] = useState(false);
     const [all, setAll] = useState(['ali', 'alireza', 'aliReza', 'Ali', 'mohammad', 'mohammadReza', 'mohsen'])
-    const debouncedSearchTerm = useDebounce(searchTerm, 322000, setIsSearching);
+    const debouncedSearchTerm = useDebounce(searchTerm, 3000, setIsSearching);
 
     useEffect(
         () => {
             setIsSearching(false);
-            if (searchTerm.length > 3) {
+            setResults([])
+            if (searchTerm.length > 2) {
                 setResults(all.filter(item => item.startsWith(searchTerm)));
             } else {
                 setResults([]);
@@ -22,24 +40,47 @@ function Main() {
         [debouncedSearchTerm]
     );
 
+    const handleAutoComplete = (e) =>{
+        e.preventDefault();
+        setSearchTerm(e.target.innerHTML)
+    }
     return (
-        <div>
-            <input
-                placeholder="Search Marvel Comics"
-                onChange={e => setSearchTerm(e.target.value)}
-            />
+        <Container fluid className="px-0 mx-0 bg-img pt-5">
 
-            <div className={searchTerm.length > 3 ? 'd-auto' : 'd-none'}>
-                {isSearching && <div>Searching ...</div>}
-                {
-                    results.map(result => (
-                        <div>
-                            <h4>{result}</h4>
+            <h2 className="text-center text-light">
+                please enter the name
+            </h2>
+            <input
+                className="w-50 mx-auto mt-5 d-flex justify-content-center my-input"
+                onChange={e => setSearchTerm(e.target.value)}
+                autoComplete="true"
+                value={searchTerm}
+            />
+            {
+                searchTerm.length > 2 ?
+                    <>
+                        {isSearching && <div>
+                            <ClipLoader
+                                css={override}
+                            />
+                        </div>}
+                        <div className="w-50 mx-auto text-white pl-3 mt-3 bg-result">
+                            {
+                                results.map((result , index) => (
+                                    <div key={index}>
+                                        <h4 className="w-50 mt-2" onClick={handleAutoComplete}>{result}</h4>
+                                    </div>
+                                ))
+                            }
                         </div>
-                    ))
-                }
-            </div>
-        </div>
+                    </> :
+                    <h2  className="w-50 mx-auto text-white pl-3 mt-3 bg-result">
+                        you should enter 3 charactor for see autocomplete
+                    </h2>
+                } 
+                
+            
+        </Container >
     );
 }
 export default Main
